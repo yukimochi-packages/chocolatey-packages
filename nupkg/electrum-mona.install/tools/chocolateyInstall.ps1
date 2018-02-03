4d421bc533b05dd3e65c10f48b96130300b44e78
Naoki Kosaka <n.k@mail.yukimochi.net>
2018-02-03 21:28:59 +0900

    Add Electrum-mona 3.0.5.

---
+++ nupkg/electrum-mona.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'electrum-mona.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/wakiyamap/electrum-mona/releases/download/3.0.5/electrum-mona-3.0.5-setup.exe'
+$checksum = '7f1be9e8599a1bd9816648e6b78f76d7171f2ea99d048de5e599985aa44f7911'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+ 
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ nupkg/electrum-mona/Changelog.md
@@ -0,0 +1,4 @@
+# Electrum-mona Changelog
+
+## Version: 3.0.5 (2018-02-03)
+- Initial implemented of the electrum-mona.install chocolatey package
